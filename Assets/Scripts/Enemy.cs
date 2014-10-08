ccd071dfea0e073a4800547dbf473e7765e7aeed
Tran MinhTuan <tuantranf@gmail.com>
2014-10-08 20:55:57 +0900

    Added score, enemy point

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,9 @@ public class Enemy : MonoBehaviour
 		// ヒットポイント
 		public int hp = 1;
 
+		// スコアのポイント
+		public int point = 100;
+
 		// Spaceshipコンポーネント
 		Spaceship spaceship;
 
@@ -68,6 +71,9 @@ public class Enemy : MonoBehaviour
 				// ヒットポイントが0以下であれば
 				if(hp <= 0 )
 				{
+						// スコアコンポーネントを取得してポイントを追加
+						FindObjectOfType<Score>().AddPoint(point);
+
 						// 爆発
 						spaceship.Explosion ();
 
--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -31,6 +31,9 @@ public class Manager : MonoBehaviour
 	
 	public void GameOver ()
 	{
+		// ハイスコアの保存
+		FindObjectOfType<Score>().Save();
+
 		// ゲームオーバー時に、タイトルを表示する
 		title.SetActive (true);
 	}
